34d1cb2759e65a70e9b234058a10c6271c08dda9
daltonhout <89536212+daltonhout@users.noreply.github.com>
2021-09-29 10:19:38 -0500

    progress

--- Lab 3-User and File IO/Main.cpp
+++ Lab 3-User and File IO/Main.cpp
@@ -2,25 +2,40 @@
 #include <cmath>
 #include <fstream>
 using namespace std;
-float findStandardDeviation(float* array, int count);
-int main()
-{
-	int count, i;
-	float inputArray[500];
+int main() {
+	string inputFileName = "C:\Users\LBDHo\source\repos\Lab 3-User and File IO\Lab 3-User and File IO";
+	string outputFileName = "C:\Users\LBDHo\source\repos\Lab 3-User and File IO\Lab 3-User and File IO";
+	ifstream infile;
+	ofstream outfile;
+	float num1;
+	float num2;
+	float num3;
+	float num4;
 
+	infile.open("inMeanStd.dat");
+	outfile.open("outMeanStd.dat");
 
 	
 	
 	
-	ofstream myfile;
-	myfile.open("inMeanStd.dat");
-	myfile << "will this work or not\n";
-	myfile.close();
+	float val[4] = { 5, 8, 9, 2 };
+	float sum = 0.0, mean, variance = 0.0, stdDeviation;
+	int i;
+	for (i = 0; i < 4; ++i)
+	sum += val[i];
+	mean = sum / 4;
+	for (i = 0; i < 4; ++i)
+		variance += pow(val[i] - mean, 2);
+	variance = variance / 4;
+	stdDeviation = sqrt(variance);
+	cout << "The data values are: ";
+	for (i = 0; i < 4; ++i)
+		cout << val[i] << " ";
+	cout << endl;
+	cout << "The standand deviation of these values is: " << stdDeviation << endl;
+	cout << "The mean of these values is: " << mean;
 
-	myfile.open("inMeanStd.dat", ios::out | ios::app | ios::binary);
-  if (myfile.is_open()) {/* ok, proceed with output */ }
+	infile.close();
+	outfile.close();
 
-	return 0;
 }
-
-	
